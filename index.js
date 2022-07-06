6cc6a487a28dcbb3e1dfabe683afce098b5d7155
Bhavya Verma <vbhavya269@gmail.com>
2022-07-06 14:19:54 +0530

    ⚡️ Getting a single member using middleware

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get('/api/members', (req, res) => {
     res.json(members);
 });
 
+// get single member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    }
+    else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
+})
+
 // set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
