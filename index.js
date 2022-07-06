e97ab32d6d0294c4c2beccf96ad6e8d2b247824d
Bhavya Verma <vbhavya269@gmail.com>
2022-07-06 12:49:12 +0530

    ⚡️ Building an API with hardcoded data

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ const path = require('path');
 
 const app = express();
 
+const members = [
+    {
+        id: 1,
+        name: 'Morty Smith',
+        email: 'morty@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Rick Sanchez',
+        email: 'getschwifty@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Summer Smith',
+        email: 'summer@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 4,
+        name: 'Beth Smith',
+        email: 'beth@gmail.com',
+        status: 'active'
+    }
+]
+
+// this route gets all members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
 // set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
