349da9e3618e55bc883c7db37cc09b9649c40a9b
Bhavya Verma <vbhavya269@gmail.com>
2022-07-06 12:01:30 +0530

    ✨ Added routes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 // if the port number is given as an environment variable.
