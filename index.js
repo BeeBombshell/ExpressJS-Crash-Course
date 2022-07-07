e48f477b9033df889eb610ef72d2d47a0e5b6b49
Bhavya Verma <vbhavya269@gmail.com>
2022-07-07 14:20:57 +0530

    ⚡️ Adding routes to API calls

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -8,25 +7,11 @@ const app = express();
 // init middleware
 app.use(logger);
 
-// this route gets all members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
-
-// get single member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id))
-
-    if (found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)))
-    }
-    else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
-    }
-})
-
 // set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// members API routes
+app.use('/api/members', require('./routes/api/members'));
 
 // if the port number is given as an environment variable.
 const PORT = process.env.PORT || 5000;
---
+++ routes/api/members.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const members = require('../../Members');
+
+
+// this route gets all members
+router.get('/', (req, res) => {
+    res.json(members);
+});
+
+// get single member
+router.get('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    }
+    else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
+})
+
+module.exports = router;
