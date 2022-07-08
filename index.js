0d1b6e42665c47e75b7b08dcd07676e3573c6717
Bhavya Verma <vbhavya269@gmail.com>
2022-07-08 17:58:27 +0530

    ✨ Added index.handlebars and homepage route

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.set('view engine', 'handlebars');
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// homepage route
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
 // set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
