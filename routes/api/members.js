f8565207994a918b764f1257a159ce4ffb133187
Bhavya Verma <vbhavya269@gmail.com>
2022-07-08 18:27:17 +0530

    ✨ Added "Visit API' button

--- routes/api/members.js
+++ routes/api/members.js
@@ -34,8 +34,8 @@ router.post('/', (req, res) => {
     }
 
     members.push(newMember);
-    // res.json(members);
-     res.redirect('/')
+    res.json(members);
+    //  res.redirect('/')
 })
 
 // update member
